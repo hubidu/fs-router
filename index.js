1976f526019ead558a6a39abb480b1afc3535395
Stefan Huber <stefan.huber@check24.de>
2017-06-16 17:32:16 +0200

    Quick hack to extract comments from route files

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const path = require('path')
 const fs = require('fs')
 const qs = require('querystring')
+const extract = require('babel-extract-comments')
+
+const extractCommentFromFile = (fileName) => {
+  const content = fs.readFileSync(fileName, 'utf8')
+  return extract(content)
+}
 
 const paramPattern = /[:%]([^/]+)/
 // takes routes and decorates them with a 'match' method that will return { params, query } if a path matches
@@ -8,7 +14,19 @@ function addMatch (route) {
   let routePath = route.path
   let paramNames = []
   let matched
-  // find any paths prefixed with a `:`, and treat them as capture groups
+
+  // Extract http method from path
+  const HTTP_METHOD_REGEX = /\.get|\.post|\.put|\.delete|\.patch/;
+  const match = routePath.match(HTTP_METHOD_REGEX)
+  if (match) {
+    route.methods = [match[0].replace('.', '').toUpperCase()]
+    routePath = routePath.replace(HTTP_METHOD_REGEX, '')
+    route.path = routePath
+  } else {
+    route.methods = ['ANY']
+  }
+
+  // find any paths prefixed with a `:` or `%` (Windows), and treat them as capture groups
   while ((matched = routePath.match(paramPattern)) !== null) {
     routePath = routePath.replace(paramPattern, '([^?/]+)')
     paramNames.push(matched[1])
@@ -32,10 +50,11 @@ function addMatch (route) {
       return { params, query }
     }
   }
-  // add supported methods
-  route.methods = typeof route === 'function' ? 
-    ['ANY'] : 
-    Object.keys(route).filter(m => ['GET', 'POST', 'PUT', 'DELETE'].indexOf(m.toUpperCase()) > -1);
+  // add supported methods to the route
+  // route.methods = typeof route === 'function' ? 
+  //   ['ANY'] : 
+  //   Object.keys(route).filter(m => ['GET', 'POST', 'PUT', 'DELETE'].indexOf(m.toUpperCase()) > -1);
+
   return route
 }
 
@@ -57,6 +76,8 @@ module.exports = function router (routesDir, config) {
     // the path is in the form of '/path/file', relative to routesDir
     .map(routeFile => {
       let route = require(routeFile)
+      route.comment = extractCommentFromFile(routeFile)
+
       let extPattern = new RegExp(path.extname(routeFile) + '$')
       if (!route.path) {
         route.path = '/' + path.relative(routesDir, routeFile).replace(extPattern, '')
@@ -74,6 +95,7 @@ module.exports = function router (routesDir, config) {
       return route
     })
     // param routes should not override specific routes (/users/login should take precedence over /users/:id)
+    // and also over /users/index
     .sort((a, b) => {
       const aa = a.path.replace(/[:%]/g, '~');
       const bb = b.path.replace(/[:%]/g, '~')
@@ -90,12 +112,20 @@ module.exports = function router (routesDir, config) {
 
   // generated match method - call with a req object to get a route.
   const matchFn = function match (req) {
-    let routeFn = r => r[req.method] || (typeof r === 'function' && r)
+    // let routeFn = r => r[req.method] || (typeof r === 'function' && r)
+    // const routeFn = r => (r.methods.indexOf(req.method) >= 0 || r.methods.indexOf('ANY') >= 0) && typeof r === 'function'
+    const routeFn = r => {
+      if (r.methods.indexOf(req.method) >= 0 || r.methods.indexOf('ANY') >= 0) {
+        return r
+      }
+      return undefined
+    }
+
     let found = routes.find(r => {
       let matched = r.match(req.url)
       let hasFn = routeFn(r)
       if (matched && hasFn) {
-        Object.assign(req, matched)
+        Object.assign(req, matched) // ???
         return true
       }
     })
